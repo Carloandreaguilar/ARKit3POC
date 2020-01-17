dcb8091e81cbde4db888ebabbfbd9359a9a36af7
candreaguilarc <candreaguilarc@bitbucket.org>
2020-01-17 15:51:12 -0500

    base project created

---
+++ ARKit3POC/ARKit3POC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKit3POC
+//
+//  Created by Carlo Andre Aguilar Castrat on 1/17/20.
+//  Copyright © 2020 Belatrix. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
